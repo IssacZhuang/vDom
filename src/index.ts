34936ef5a99f361eda80285e2e85eeb1e53dc1c6
Issac Zhuang <1095313400@qq.com>
2019-08-10 01:06:44 +0800

    优化了事件绑定;新增cloneNode方法;修复渲染时Dom节点指向不正确的bug,防止内存溢出

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,22 @@ import { createNode as e } from './vdom/vnode'
 import vDom from './vdom/vdom'
 
 window.addEventListener('load', () => {
+
+    type test={
+        a:string
+        b?:test
+    }
     
-    const testFunction = function(){
+    const testFunction = function(e:Event){
         console.log(this);
+        console.log(e)
     }
 
     const vdom = new vDom('app');
     const node1 =
         e('p', { class: 'test' }, [
             'title',
-            e('div', { class: 'btn primary', onclick: testFunction }, ['save'])
+            e('div', { class: 'btn primary', click: testFunction }, ['save'])
         ]);
 
     vdom.render([node1]);
