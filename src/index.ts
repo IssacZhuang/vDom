073e1989f72351d63d4abeb607123ce0f2eb5533
Issac Zhuang <1095313400@qq.com>
2019-08-10 23:24:39 +0800

    vNode可以作为参数传入事件绑定函数

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-import { createNode as e } from './vdom/vnode'
+import vNode, { createNode as e } from './vdom/vnode'
 import vDom from './vdom/vdom'
 
 window.addEventListener('load', () => {
 
-    type test={
-        a:string
-        b?:test
+    type test = {
+        a: string
+        b?: test
     }
-    
-    const testFunction = function(e:Event){
+
+    const testFunction = function (node: vNode, e: Event) {
+        console.log(node);
+        console.log(e);
         console.log(this);
-        console.log(e)
     }
 
     const vdom = new vDom('app');
